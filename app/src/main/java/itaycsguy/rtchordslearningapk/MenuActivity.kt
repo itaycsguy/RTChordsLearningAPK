22fc4abe4268a9199913227e25143091c7f059fe
Morag Liraz <morag141@gmail.com>
2018-08-23 16:57:12 +0300

    Made work on the Menu Activity design
    * added 2 more buttons - edit and camera
    * added a new not implemenet msg on not working
    * started working on permissions (added camera, and extral data)
    
    * user can now take a picture using our app in order to upload it in the future.

--- app/src/main/java/itaycsguy/rtchordslearningapk/MenuActivity.kt
+++ app/src/main/java/itaycsguy/rtchordslearningapk/MenuActivity.kt
@@ -2,33 +2,72 @@ package itaycsguy.rtchordslearningapk
 
 import android.app.Activity
 import android.content.Intent
-import android.net.Uri
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.support.v7.app.AppCompatDialogFragment
+import android.support.v7.app.AppCompatActivity
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.R.attr.duration
+import android.support.design.widget.Snackbar
+import android.support.design.widget.Snackbar.LENGTH_SHORT
+import android.view.View
+import android.graphics.Bitmap
+import android.R.attr.data
+import android.support.v4.app.NotificationCompat.getExtras
+
+
+
 
 class MenuActivity : AppCompatActivity() {
-
+    /*
+    Variables of the activity
+     */
     lateinit var searchButton : ImageButton
     lateinit var uploadButton: ImageButton
+    lateinit var editButton: ImageButton
+    lateinit var cameraButton: ImageButton
     lateinit var imageView : ImageView
-//    private lateinit var imageUri: Uri
+    lateinit var cordinatorView : View
 
+    /*
+    Const values for result
+     */
     val PICK_IMAGE = 100
+    val REQUEST_IMAGE_CAPTURE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.menu_activity)
+        cordinatorView = findViewById(R.id.myCoordinatorLayout)
         searchButton = findViewById(R.id.SearchButton)
+
         uploadButton = findViewById(R.id.UploadButton)
+        uploadButton.setOnClickListener{
+            Snackbar.make(cordinatorView, "Sorry Not Implemented Yet!", LENGTH_SHORT).show()
+        }
+
+        editButton = findViewById(R.id.EditButton)
+        editButton.setOnClickListener{
+            Snackbar.make(cordinatorView, "Sorry Not Implemented Yet!", LENGTH_SHORT).show()
+        }
+        cameraButton = findViewById(R.id.CameraButoon)
+        cameraButton.setOnClickListener {
+            dispatchTakePictureIntent()
+        }
         imageView = findViewById(R.id.UploadedView)
 
         searchButton.setOnClickListener {
             openGallery()
         }
 
+    }
+
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(packageManager) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        }
     }
 
     private fun openGallery(){
@@ -39,8 +78,12 @@ class MenuActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode : Int, resultCode : Int, data : Intent){
         super.onActivityResult(requestCode, resultCode, data)
         if ((resultCode == Activity.RESULT_OK).and(requestCode == PICK_IMAGE)){
-//            imageUri = data.data
             imageView.setImageURI(data.data)
+        }
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            val extras = data.extras
+            val imageBitmap = extras?.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitmap)
         }
 
     }
