72b1e599d55ef46c9ae366da38b32c85288e4759
Morag Liraz <morag141@gmail.com>
2018-08-30 13:55:52 +0300

    Alot of mess just uploading to the cloud.

--- app/src/main/java/itaycsguy/rtchordslearningapk/MenuActivity.kt
+++ app/src/main/java/itaycsguy/rtchordslearningapk/MenuActivity.kt
@@ -1,24 +1,34 @@
 package itaycsguy.rtchordslearningapk
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.provider.MediaStore
-import android.support.v7.app.AppCompatDialogFragment
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.app.AppCompatDialogFragment
 import android.widget.ImageButton
 import android.widget.ImageView
-import android.R.attr.duration
 import android.support.design.widget.Snackbar
 import android.support.design.widget.Snackbar.LENGTH_SHORT
 import android.view.View
 import android.graphics.Bitmap
-import android.R.attr.data
+import android.net.Uri
+import android.os.Environment
 import android.support.v4.app.NotificationCompat.getExtras
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.util.DisplayMetrics
+import android.support.v7.widget.Toolbar
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.menu_activity.*
+import java.io.File
+import java.util.jar.Manifest
 
-
-
-
+@SuppressLint("ByteOrderMark")
 class MenuActivity : AppCompatActivity() {
     /*
     Variables of the activity
@@ -29,45 +39,92 @@ class MenuActivity : AppCompatActivity() {
     lateinit var cameraButton: ImageButton
     lateinit var imageView : ImageView
     lateinit var cordinatorView : View
+    /*
+    Testing for crop
+     */
+    lateinit var file : File
 
+    lateinit var cropIntent : Intent
+    lateinit var displayMetrics : DisplayMetrics
+    var width : Int = 0
+    var height : Int = 0
+    lateinit var toolbar : Toolbar
     /*
     Const values for result
      */
     val PICK_IMAGE = 100
-    val REQUEST_IMAGE_CAPTURE = 1
+
+    private val TAG = "Permissions"
+    private val REQUEST_IMAGE_CAPTURE = 1
+    private val REQUEST_PERMISSION_CODE = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.menu_activity)
         cordinatorView = findViewById(R.id.myCoordinatorLayout)
-        searchButton = findViewById(R.id.SearchButton)
+//        searchButton = findViewById(R.id.SearchButton)
 
-        uploadButton = findViewById(R.id.UploadButton)
-        uploadButton.setOnClickListener{
-            Snackbar.make(cordinatorView, "Sorry Not Implemented Yet!", LENGTH_SHORT).show()
+        toolbar = findViewById(R.id.toolbar)
+        toolbar.title = ("Choose Operation")
+        setSupportActionBar(toolbar)
+        var permissionCheck = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+        if (permissionCheck == PackageManager.PERMISSION_DENIED){
+            Log.i(TAG, "Permission to use camera denied")
+            makeRequest()
         }
-
-        editButton = findViewById(R.id.EditButton)
-        editButton.setOnClickListener{
-            Snackbar.make(cordinatorView, "Sorry Not Implemented Yet!", LENGTH_SHORT).show()
-        }
-        cameraButton = findViewById(R.id.CameraButoon)
-        cameraButton.setOnClickListener {
-            dispatchTakePictureIntent()
-        }
+//        uploadButton = findViewById(R.id.UploadButton)
+//        uploadButton.setOnClickListener{
+//            Snackbar.make(cordinatorView, "Sorry Not Implemented Yet!", LENGTH_SHORT).show()
+//        }
+//
+//        editButton = findViewById(R.id.EditButton)
+//        editButton.setOnClickListener{
+//            Snackbar.make(cordinatorView, "Sorry Not Implemented Yet!", LENGTH_SHORT).show()
+//        }
+//        cameraButton = findViewById(R.id.CameraButoon)
+//        cameraButton.setOnClickListener {
+//            openCamera()
+//        }
         imageView = findViewById(R.id.UploadedView)
 
-        searchButton.setOnClickListener {
-            openGallery()
-        }
+//        searchButton.setOnClickListener {
+//            openGallery()
+//        }
 
     }
 
-    private fun dispatchTakePictureIntent() {
-        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.CAMERA),
+                REQUEST_PERMISSION_CODE)
+    }
+
+//    ﻿override fun onRequestPermissionsResult(requestCode: Int,
+//                                             permissions: Array<String>, grantResults: IntArray) {
+//        when (requestCode) {
+//            REQUEST_PERMISSION_CODE -> {
+//                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+//                    Log.i(TAG, "Permission has been denied by user")
+//                } else {
+//                    Log.i(TAG, "Permission has been granted by user")
+//                }
+//            }
+//        }
+//    }﻿
+    private lateinit var uri: Uri
+    private lateinit var takePictureIntent: Intent
+
+    private fun openCamera() {
+//        file = File(Environment.getExternalStorageDirectory(),
+//                "file ${System.currentTimeMillis()}.jpg")
+//        uri = Uri.fromFile(file)
+        takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
+//        takePictureIntent.putExtra("return-data", true)
         if (takePictureIntent.resolveActivity(packageManager) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
         }
+
     }
 
     private fun openGallery(){
@@ -88,7 +145,25 @@ class MenuActivity : AppCompatActivity() {
 
     }
 
-//    TODO:This is how to swap activities!
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        item?.let {
+            if (item.itemId == R.id.btn_camera){
+                openCamera()
+            } else if (item.itemId == R.id.btn_gallery){
+                openGallery()
+            }
+        }
+        return false
+    }
+
+
+
+    //    TODO:This is how to swap activities!
 //    fun activitySwap(){
 //        startActivity(Intent(this, secondActivity::class.java))
 //    }
