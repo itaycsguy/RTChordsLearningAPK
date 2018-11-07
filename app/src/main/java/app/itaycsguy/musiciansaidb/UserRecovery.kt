63b7e64c5203e2bafa977a3a973876425619ada6
itaycsguy <itgu1010@gmail.com>
2018-11-08 01:18:10 +0200

    fixed recovery functionality: it is sending the password to user email address if he exists, otherwise it is notify about email not exists problem

--- app/src/main/java/app/itaycsguy/musiciansaidb/UserRecovery.kt
+++ app/src/main/java/app/itaycsguy/musiciansaidb/UserRecovery.kt
@@ -4,14 +4,25 @@ import android.support.v7.app.AppCompatActivity
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ProgressBar
+import app.itaycsguy.musiciansaidb.R.id.progressBar
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import java.util.*
+import javax.mail.Authenticator
+import javax.mail.PasswordAuthentication
+import javax.mail.Session
 
 
-class UserRecovery(act : AppCompatActivity,fbAuth : FirebaseAuth) : TextWatcher {
+@Suppress("NAME_SHADOWING")
+class UserRecovery(act : AppCompatActivity, fbDb: FirebaseDB) : TextWatcher {
     private val REQUESTCODE = 6
     private val _act : StartActivity = act as StartActivity
-    private val _fbAuth = fbAuth
+    private val _fbDb = fbDb
 
     fun initOperations() {
         _act.findViewById<EditText>(R.id.text_email_recovery).addTextChangedListener(this)
@@ -21,7 +32,10 @@ class UserRecovery(act : AppCompatActivity,fbAuth : FirebaseAuth) : TextWatcher 
             if(emailAddrText.isNotEmpty()) {
                 hideKeyboard(_act)
                 this.sendEmail(emailAddrText)
-            } else { CustomSnackBar.make(_act,  "Email address is missing!") }
+            } else {
+                _act.findViewById<ProgressBar>(R.id.recovery_progressBar).visibility = View.INVISIBLE
+                CustomSnackBar.make(_act,  "Email address is missing!")
+            }
         }
         (_act.findViewById<Button>(R.id.cancel_recovery_button)).setOnClickListener { _act.showLogin() }
     }
@@ -35,15 +49,55 @@ class UserRecovery(act : AppCompatActivity,fbAuth : FirebaseAuth) : TextWatcher 
     override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
     override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
+    /*
+    smtp domain: smtp.gmail.com
+    smtp email: "musical.lab100@gmail.com"
+    smtp password: "@123mlab"
+     */
     private fun sendEmail(email : String) {
         try {
-            val thread = Runnable {
-                val sender = GMailSender("musical.lab100@gmail.com", "@123mlab")
-                sender.sendMail("This is Subject", "This is Body", email, "")
+            val progressBar = startProgressBar(_act,R.id.recovery_progressBar)
+            val fromEmail = "musical.lab100@gmail.com" //requires valid gmail id
+            val password = "@123mlab" // correct password for gmail id
+
+            System.out.println("TLSEmail Start")
+            val props = Properties()
+            props["mail.smtp.host"] = "smtp.gmail.com" //SMTP Host
+            props["mail.smtp.port"] = "587" //TLS Port
+            props["mail.smtp.auth"] = "true" //enable authentication
+            props["mail.smtp.starttls.enable"] = "true" //enable STARTTLS
+
+            //create Authenticator object to pass in Session.getInstance argument
+            val auth = object : Authenticator() {
+                //override the getPasswordAuthentication method
+                 override fun getPasswordAuthentication() : PasswordAuthentication {
+                    return PasswordAuthentication(fromEmail, password)
+                }
             }
-            thread.run()
+            val session = Session.getInstance(props, auth)
+            _fbDb.getRef("users/${FirebaseDB.encodeUserEmail(email)}")?.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onCancelled(p0: DatabaseError) {}
+
+                override fun onDataChange(p0: DataSnapshot) {
+                    if(p0.exists()) {
+                        val provider = p0.child("authentication_vendor").value.toString().toLowerCase()
+                        var password = "Gmail account"
+                        if (provider == "app") {
+                            password = p0.child("password").value.toString()
+                        }
+                        EmailUtil.sendEmail(session, email, "Musical DB - Password Remainder", "Your password is: $password")
+                        CustomSnackBar.make(_act, "Email is sent! Check your mail-box...")
+                        stopProgressBar(progressBar)
+                        _act.showLogin()
+                    } else {
+                        CustomSnackBar.make(_act,  "Email address is incorrect/cannot be reached!")
+                    }
+                    stopProgressBar(progressBar)
+                }
+            })
         } catch (e: Exception) {
-            Log.e("SendMail", e.message, e)
+            Log.e("SendEmail", e.message, e)
+            throw e
         }
 
     }
